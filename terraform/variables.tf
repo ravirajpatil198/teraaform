b64157754e6fe0e9a6328362a87c4d8a2ba49dc4
raviraj patil <143616067+ravirajpatil198@users.noreply.github.com>
2026-07-08 17:40:23 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -23,7 +23,7 @@ variable "env_config" {
     prod = {
       vpc_cidr      = "10.3.0.0/16"
       subnet_cidr   = "10.3.1.0/24"
-      instance_type = "t3.medium"
+      instance_type = "t3.micro"
     }
   }
 }
