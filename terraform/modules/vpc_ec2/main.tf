a4ce4c49069e164ebac2a4f601ca65c44ce34f5c
raviraj patil <143616067+ravirajpatil198@users.noreply.github.com>
2026-07-08 17:56:49 +0530

    Update main.tf

--- terraform/modules/vpc_ec2/main.tf
+++ terraform/modules/vpc_ec2/main.tf
@@ -15,6 +15,7 @@ resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.subnet_cidr
   map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
   tags                    = { Name = "${var.env}-public-subnet" }
 }
 
